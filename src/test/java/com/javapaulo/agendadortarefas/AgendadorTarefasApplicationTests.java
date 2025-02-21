8f30c5dd03420357b4e82cfecb651cfce9d5b13b
paulorsp2021 <paulorsp2021@gmail.com>
2025-02-21 16:49:42 -0300

    commit inicial com esqueleto

---
+++ src/test/java/com/javapaulo/agendadortarefas/AgendadorTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javapaulo.agendadortarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendadorTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

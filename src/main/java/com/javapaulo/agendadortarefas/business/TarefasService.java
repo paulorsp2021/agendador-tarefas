5f11e4f58cb531b1c224404d231bafa42717020e
paulorsp2021 <paulorsp2021@gmail.com>
2025-02-24 15:29:56 -0300

    adic metodo Get por email e get tarefas por periodo

--- src/main/java/com/javapaulo/agendadortarefas/business/TarefasService.java
+++ src/main/java/com/javapaulo/agendadortarefas/business/TarefasService.java
@@ -10,6 +10,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -26,5 +27,17 @@ public class TarefasService {
         dto.setEmailUsuario(email);
         TarefasEntity entity = tarefasConverter.paraTarefaEntity(dto);
         return tarefasConverter.paraTarefaDTO(tarefasRepository.save(entity));
+    }
+
+    public List<TarefasDTO> buscaTarefasAgendadasPorPeriodo(LocalDateTime dataInicial, LocalDateTime dataFinal){
+
+        return tarefasConverter.paraListaTarefasDTO(tarefasRepository.findByDataEventoBetween(dataInicial, dataFinal));
+    }
+
+    public List<TarefasDTO> buscaTarefasPorEmail(String token){
+        String email = jwtUtil.extractEmailToken(token.substring(7));
+        List<TarefasEntity> listaTarefas = tarefasRepository.findByEmailUsuario(email);
+
+        return tarefasConverter.paraListaTarefasDTO(listaTarefas);
     }
 }
--- src/main/java/com/javapaulo/agendadortarefas/business/mapper/TarefasConverter.java
+++ src/main/java/com/javapaulo/agendadortarefas/business/mapper/TarefasConverter.java
@@ -4,10 +4,16 @@ import com.javapaulo.agendadortarefas.Infrastructure.entity.TarefasEntity;
 import com.javapaulo.agendadortarefas.business.dto.TarefasDTO;
 import org.mapstruct.Mapper;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring")
 public interface TarefasConverter {
 
     TarefasEntity paraTarefaEntity(TarefasDTO dto);
 
     TarefasDTO paraTarefaDTO(TarefasEntity entity);
+
+    List<TarefasEntity> paraListaTarefasEntity(List<TarefasDTO> dtos);
+
+    List<TarefasDTO> paraListaTarefasDTO(List<TarefasEntity> entities);
 }

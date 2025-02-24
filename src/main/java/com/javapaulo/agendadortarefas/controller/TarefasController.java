93c0063152e3f4f06756c328f2415a18366ac238
paulorsp2021 <paulorsp2021@gmail.com>
2025-02-24 16:27:10 -0300

    adic metodo Patch e Put

--- src/main/java/com/javapaulo/agendadortarefas/controller/TarefasController.java
+++ src/main/java/com/javapaulo/agendadortarefas/controller/TarefasController.java
@@ -1,5 +1,6 @@
 package com.javapaulo.agendadortarefas.controller;
 
+import com.javapaulo.agendadortarefas.Infrastructure.enums.StatusNotificationEnum;
 import com.javapaulo.agendadortarefas.business.TarefasService;
 import com.javapaulo.agendadortarefas.business.dto.TarefasDTO;
 import lombok.RequiredArgsConstructor;
@@ -35,6 +36,23 @@ public class TarefasController {
     public ResponseEntity<List<TarefasDTO>> buscaTarefasPorEmail(@RequestHeader("Authorization") String token){
 
         return ResponseEntity.ok(tarefasService.buscaTarefasPorEmail(token));
-
     }
+    @DeleteMapping
+    public ResponseEntity<Void> deletaTarefaPorId(@RequestParam("id") String id){
+
+        return ResponseEntity.ok().build();
+    }
+
+    @PatchMapping
+    public ResponseEntity<TarefasDTO> alteraStatusNotificacao(@RequestParam("status") StatusNotificationEnum status,
+                                                              @RequestParam("id") String id){
+        return ResponseEntity.ok(tarefasService.alteraStatus(status, id));
+    }
+
+    @PutMapping
+    public ResponseEntity<TarefasDTO> updateTarefas(@RequestBody TarefasDTO dto,
+                                                    @RequestParam("id") String id){
+        return ResponseEntity.ok(tarefasService.updateTarefas(dto, id));
+    }
+
 }
